c3e4cf608672f33d68fd9ff1a088deecde58634c
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2023-07-27 10:56:20 -0300

    Double quotes and other improvements.

--- ora-ecl.js
+++ ora-ecl.js
@@ -9,23 +9,37 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 function toEclipse(src, dest) {
-	dest.value = src.value.split('\n').map((line, i) => {
-		const li = line.replace('siorg.', '{h-schema}');
-		return i == 0
-			? `String sql = "${li}"`
-			: `+ " ${li}"`;
-	}).join('\n') + ';';
+	dest.value = src.value.split('\n')
+		.map((line, i) => {
+			const li = line.replace('siorg.', '{h-schema}')
+				.replaceAll('\"', '\\\"');
+
+			return (i == 0)
+				? `String sql = "${li}"`
+				: `+ " ${li}"`;
+		})
+		.join('\n') + ';';
+
 	dest.select();
 	dest.focus();
 }
 
 function toOracle(src, dest) {
-	dest.value = src.value.split('\n').map((line, i) => {
-		return line.replace('String sql = "', '')
-			.replace('{h-schema}', 'siorg.')
-			.replace(/^\s+\+ \"/, '')
-			.replace(/";?$/, '')
-	}).join('\n');
+	dest.value = src.value.split('\n')
+		.map(line => {
+			const commIdx = line.lastIndexOf('//');
+			if (commIdx !== -1) {
+				line = line.substring(0, commIdx);
+			}
+
+			return line.replace(/\s*String\s*sql\s*=\s*"/, '')
+				.replace('{h-schema}', 'siorg.')
+				.replaceAll('\\\"', '\"')
+				.replace(/^\s+\+ \"/, '')
+				.replace(/";?\s*$/, '');
+		})
+		.join('\n');
+
 	dest.select();
 	dest.focus();
 }
